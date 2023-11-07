2fc147d278f2b54674db55599701c7a6b51dd1c8
David Mason <DIMason1702@gmail.com>
2023-11-07 21:40:17 +0000

    Server.js complete

---
+++ Backend/server.js
@@ -0,0 +1,11 @@
+// Import app
+import app from "./app.js"
+
+// Declare a variable for the port from .env
+const port = process.env.PORT ?? 4000;
+
+// Set the server to listen to port
+app.listen(port, () => {
+    // Console log to show which port is being used.
+    console.log(`Server is listening on http://localhost:${port}`);
+})
