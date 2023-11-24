80e551f86cf5cdc85c0abb29fd212827734357ba
David Mason <DIMason1702@gmail.com>
2023-11-24 18:37:52 +0000

    Post list filling complete

--- Frontend/main.js
+++ Frontend/main.js
@@ -1,4 +1,5 @@
-// // DOM items
+// // // DOM items
+// // Header items
 // Login button
 const LOGIN_BUTTON = document.getElementById("login-button");
 // Login section
@@ -13,9 +14,19 @@ const SIGNED_IN_SECTION = document.getElementById("signed_in_options")
 const SIGN_IN_BUTTON = document.getElementById("sign_in");
 // Logout button
 const LOGOUT_BUTTON = document.getElementById("logout-button");
+
+// // Main items
+// Post list section
+const POST_LIST = document.getElementById("post_list")
+
+// // Admin items
 // Admin section
 const ADMIN_SECTION = document.getElementById("admin_section");
+// Create post header
+const CREATE_POST_TITLE = document.getElementById("create_post")
 
+
+// API endpoint
 const apiEndpoint = "http://localhost:7171";
 
 // // Login Section
@@ -43,6 +54,7 @@ async function userLogin (username, password) {
     // Parse the response
     const data = await response.json();
 
+    //Return the data
     return data.data
 }
 
@@ -95,3 +107,53 @@ LOGOUT_BUTTON.addEventListener("click", () => {
     // Admin section disappears
     ADMIN_SECTION.style.display = "none";
 })
+
+
+// // Post List Section
+// Function to get all posts
+async function getPosts() {
+    // Send request to API
+    const response = await fetch(apiEndpoint + "/posts");
+
+    // Parse the response
+    const data = await response.json();
+
+    // Return the data
+    return data.data
+}
+
+// Eventt listener for the page loading
+window.addEventListener("load", async () => {
+    // Call fetch functtion and set to variable
+    const posts = await getPosts();
+
+    // Loop through the posts and create articles
+    posts.map((elem) => {
+        // Create article
+        const article = document.createElement("article");
+
+        // Set article ID to post ID
+        article.id = elem.id;
+
+        // Create title tag
+        const title = document.createElement("h4");
+
+        // Set title content
+        title.textContent = `Day ${elem.day} of 100:`;
+
+        // Create post content tag
+        const content = document.createElement("p");
+
+        // Set post content's content
+        content.textContent = `${elem.post}`;
+
+        // Appaend the title and content to article
+        article.append(title, content);
+
+        // Append article to the post list
+        POST_LIST.appendChild(article);
+    });
+})
+
+
+// // Admin Section
