f85e0570a28e062438a52b8e9b242aa0c2ab6305
David Mason <david_mason_@hotmail.co.uk>
2023-12-19 19:14:04 +0000

    Extra push due to files not added

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-// // Imports
-// Import express
-import express from "express";
+// // // Imports
+// // Import express
+// import express from "express";
 
-// Import cors
-import cors from "cors";
+// // Import cors
+// import cors from "cors";
 
-// Import posts routes
-import {postsRoutes} from "./Routes/postsRoutes.js";
+// // Import posts routes
+// import {postsRoutes} from "./Routes/postsRoutes.js";
 
-// Import login route(s)
-import {loginRoutes} from "./Routes/loginRoutes.js";
+// // Import login route(s)
+// import {loginRoutes} from "./Routes/loginRoutes.js";
 
-// Declare the app variable set to express
-const app = express();
-app.use(express.static("public"))
+// // Declare the app variable set to express
+// const app = express();
+// app.use(express.static("public"))
 
-// Set the app.use methods
-app.use(cors());
-app.use(express.json());
-app.use("/posts", postsRoutes);
-app.use("/login", loginRoutes);
+// // Set the app.use methods
+// app.use(cors());
+// app.use(express.json());
+// app.use("/posts", postsRoutes);
+// app.use("/login", loginRoutes);
 
-// Error handling for use of an incorrect filepath
-app.use((_req, res, _next) => {
-    res.status(404).json({
-        success: false,
-        error: "Incorrect file path. Please try /posts"
-    });
-});
+// // Error handling for use of an incorrect filepath
+// app.use((_req, res, _next) => {
+//     res.status(404).json({
+//         success: false,
+//         error: "Incorrect file path. Please try /posts"
+//     });
+// });
 
-// Export the app variable as default
-export default app;
+// // Export the app variable as default
+// export default app;
